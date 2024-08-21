d897b3ffd77134bc7e6a53d9131159462dd48ff4
revising <vipanshu.srivastava28@gmail.com>
2024-08-21 11:01:16 +0530

    completed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+//socket.io
+io.on("connection", (socket) => {
+	socket.on("user-message", (message) => {
+		io.emit("message", message);
+	});
+});
 
 app.use(express.static(path.resolve("./public")));
 
